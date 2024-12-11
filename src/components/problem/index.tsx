8c0d34a471130337918fc13aa858c990eb159926
saxenar96 <saxena12.purdue@gmail.com>
2024-12-11 01:30:46 -0600

    fix issue with Problem tabs

--- src/components/problem/index.tsx
+++ src/components/problem/index.tsx
@@ -15,7 +15,6 @@ const problemTabs = ['Description', 'Output', 'Solution', 'Editorial']
 export default function Problem(props: ProblemProps) {
     const { title, description, devComponent: Dev, solnComponent: Soln } = props
     const [ leftTab, setLeftTab ] = useState(problemTabs[0])
-    const [ leftTabContent, setLeftTabContent ] = useState<ReactElement | null>(null)
 
     const [ codeString, setCodeString ] = useState(defaultCodeSnippet)
     const [ codeContent, setCodeContent ] = useState<ReactElement | null>(null)
@@ -24,44 +23,29 @@ export default function Problem(props: ProblemProps) {
         setLeftTab(data)
     }
 
-    useEffect(() => {
-        let content = null
-        switch(leftTab) {
-            case 'Description':
-                content = (
-                    <ReactMarkdown>
-                        { description }
-                    </ReactMarkdown>
-                )
-                break;
-            case 'Solution':
-                content = (
-                    <SolutionTab
-                        title='Solution'
-                        description="The expected output from the problem"
-                        key='problem_card_soln'
-                        component={Soln}
-                    />
-                )
-                break;
-            case 'Output':
-                content = codeContent
-                break;
-            default:
-                content = (
-                    <>Something Else</>
-                )
-        }
-
-        if (content) {
-            setLeftTabContent(content)
-        }
-    }, [leftTab])
+    const tabMap = {
+        'Description': (
+            <ReactMarkdown>
+                { description }
+            </ReactMarkdown>
+        ),
+        'Solution': (
+            <SolutionTab
+                title='Solution'
+                description="The expected output from the problem"
+                key='problem_card_soln'
+                component={Soln}
+            />
+        ),
+        'Output': codeContent,
+        'Editorial': (
+            <>Something Else</>
+        )
+    }
 
     useEffect(() => {
         const content = renderComponentFromString(codeString)
         if (content) {
-            console.log('New Content!')
             setCodeContent(content)
             setLeftTab(problemTabs[1])
         }
@@ -69,9 +53,6 @@ export default function Problem(props: ProblemProps) {
 
     const handleDevCodeChange = (code: string) => {
         setCodeString(code)
-        setTimeout(() => {
-            setLeftTabContent(codeContent)
-        }, 500)
     }
 
     useEffect(() => {
@@ -87,17 +68,19 @@ export default function Problem(props: ProblemProps) {
                 >
                     <TabsList>
                         {
-                            problemTabs.map(problem => (
-                                <TabsTrigger key={problem} value={problem}>
-                                    <TabsContent value={leftTab}>
-                                        { problem }
-                                    </TabsContent>
-                                </TabsTrigger>
+                            problemTabs.map(tabName => (
+                                <TabsTrigger key={tabName} value={tabName}>{tabName}</TabsTrigger>
                             ))
                         }
                     </TabsList>
+                    {
+                        problemTabs.map(tab => (
+                            <TabsContent value={tab}>
+                                { tabMap[tab] }
+                            </TabsContent>
+                        ))
+                    }
                 </Tabs>
-                { leftTabContent }
             </div>
             <div className='flex flex-col gap-[16px] w-full min-w-[490px]'>
                 <CodeEditor
