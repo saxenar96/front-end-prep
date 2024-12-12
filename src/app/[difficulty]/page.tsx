5383591bae63f0211e60009542334f955e2f6fc8
saxenar96 <saxena12.purdue@gmail.com>
2024-12-12 17:51:26 -0600

    add basic difficulty page

--- src/app/[difficulty]/page.tsx
+++ src/app/[difficulty]/page.tsx
@@ -1,12 +1,35 @@
+import { Button } from "@/components/ui/button"
+import { getProblemsByDifficulty } from "@/const/problems"
+import { DIFFICULTY_TITLES, PROBLEM_DIFFICULTY } from "@/types/problem"
+import Link from "next/link"
+
+function getDiffTitle(diff: string) {
+    if (diff === 'easy') return DIFFICULTY_TITLES.easy
+    if (diff === 'medium') return DIFFICULTY_TITLES.medium
+    return DIFFICULTY_TITLES.hard
+}
+
 export default async function ProblemPage({
     params 
 }: {
     params: Promise<{ difficulty: string }>
 }) {
-    const difficulty = (await params).difficulty
+    const { difficulty } = (await params)
+    const title = getDiffTitle(difficulty)
+    const problemSet = getProblemsByDifficulty(PROBLEM_DIFFICULTY[title])
+    
     return (
         <div>
-            { difficulty }
+            <h1 className="text-[2em] pb-[0.3em] font-bold">{ `${title} Problems` }</h1>
+            <div className="flex flex-col gap-1 justify-start items-start">
+                {
+                    problemSet.map(problem => (
+                        <Button variant="link" asChild>
+                            <Link href={problem.url}>{problem.title}</Link>
+                        </Button>
+                    ))
+                }
+            </div>
         </div>
     )
 }
