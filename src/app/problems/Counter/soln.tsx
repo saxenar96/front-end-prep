260a9b42417145f7d37da8424d26a6433941f698
saxenar96 <saxena12.purdue@gmail.com>
2025-01-12 11:41:32 -0800

    remove react from counter

--- src/app/problems/Counter/soln.tsx
+++ src/app/problems/Counter/soln.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import React from 'react'
+// import React from 'react'
 import './soln.css'
 
 export default function Soln(): JSX.Element {
-    const [count, setCount] = React.useState(0)
+    // const [count, setCount] = React.useState(0)
 
     const handleClick = () => {
-        setCount(count + 1)
+        // setCount(count + 1)
     }
     return (
         <div className='container'>
-            <h1>{ count }</h1>
+            {/* <h1>{ count }</h1> */}
             <button onClick={handleClick}>Increment</button>
         </div>
     )
