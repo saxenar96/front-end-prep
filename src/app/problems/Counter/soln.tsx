319510278ba2a6235e1334c6e618dc5cbaa96614
saxenar96 <saxena12.purdue@gmail.com>
2025-01-12 15:18:37 -0800

    add default code

--- src/app/problems/Counter/soln.tsx
+++ src/app/problems/Counter/soln.tsx
@@ -1,18 +1,28 @@
+// 'use client'
+
+// import React from 'react'
+// import './soln.css'
+
+// export default function Soln(): JSX.Element {
+//     const [count, setCount] = React.useState(0)
+
+//     const handleClick = () => {
+//         setCount(count + 1)
+//     }
+//     return (
+//         <div className='container'>
+//             <h1>{ count }</h1>
+//             <button onClick={handleClick}>Increment</button>
+//         </div>
+//     )
+// }
+
 'use client'
 
-import React from 'react'
 import './soln.css'
 
 export default function Soln(): JSX.Element {
-    const [count, setCount] = React.useState(0)
-
-    const handleClick = () => {
-        setCount(count + 1)
-    }
     return (
-        <div className='container'>
-            <h1>{ count }</h1>
-            <button onClick={handleClick}>Increment</button>
-        </div>
+        <div>Hello World</div>
     )
 }
