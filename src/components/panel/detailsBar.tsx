a4c990250cf78e0017fa940e593b840183cc2047
saxenar96 <saxena12.purdue@gmail.com>
2025-02-24 14:58:05 -0800

    Clean up styles and difficulty text in details bar

--- src/components/panel/detailsBar.tsx
+++ src/components/panel/detailsBar.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PanelTabAdditionalDetailsProps } from "./config";
 import { Clock, BicepsFlexed, Atom, Code } from 'lucide-react';
 import { PROBLEM_TYPES } from '@/types/problem';
 
+function cleanDiffString(str?: string): string | undefined {
+    return str !== undefined ? str.charAt(0).toUpperCase() + String(str).slice(1) : undefined
+}
+
 export function DetailsBar(props: Partial<PanelTabAdditionalDetailsProps>) {
+    const [difficultyString ] = useState<string | undefined>(cleanDiffString(props.difficulty))
     return (
-        <div className='w-full flex justify-between py-2'>
+        <div className='w-full flex gap-16 py-2'>
             <div className='flex gap-2'>
                 <BicepsFlexed />
-                { props.difficulty }
+                { difficultyString }
             </div>
             <div className='flex gap-2'>
                 <Clock />
